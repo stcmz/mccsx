f4622abfb61985992a55ee51a4973daa7ac12f67
Maozi Chen <maozichen@pitt.edu>
2020-09-26 19:25:26 -0400

    Improved Logger thread safety.

--- Helpers/Logger.cs
+++ Helpers/Logger.cs
@@ -4,23 +4,34 @@ namespace mccsx.Helpers
 {
     internal static class Logger
     {
+        private static readonly object lockObj = new object();
+
         public static void Info(string message)
         {
-            Console.WriteLine(message);
+            lock (lockObj)
+            {
+                Console.WriteLine(message);
+            }
         }
 
         public static void Warning(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Error.WriteLine($"WARN: {message}");
-            Console.ResetColor();
+            lock (lockObj)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Error.WriteLine($"WARN: {message}");
+                Console.ResetColor();
+            }
         }
 
         public static void Error(string message)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Error.WriteLine($"ERROR: {message}");
-            Console.ResetColor();
+            lock (lockObj)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine($"ERROR: {message}");
+                Console.ResetColor();
+            }
         }
     }
 }
